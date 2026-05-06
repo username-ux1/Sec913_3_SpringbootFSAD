b774a8663fba3d5d541a5292d3e89d668e26ea9d
P Sai Roshini <250030334@kluniversity.in>
2026-05-06 19:24:01 +0530

    your message

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,6 +13,7 @@ const App = () => {
         fullname: "",
         phone: "",
         email: "",
+        role: "",
         password: "",
         retypepassword: ""
     });
@@ -29,6 +30,7 @@ const App = () => {
     function switchWindow(){
         setIsSignIn(prev => !prev);
         setErrorData({});
+
         setSigninData({
             username: "",
             password: ""
@@ -38,6 +40,7 @@ const App = () => {
             fullname: "",
             phone: "",
             email: "",
+            role: "",
             password: "",
             retypepassword: ""
         });
@@ -54,127 +57,345 @@ const App = () => {
     }
 
     function validateSignup(){
+
         let errors = {};
-        if(signupData.fullname === "") errors.fullname = true;
-        if(signupData.phone === "") errors.phone = true;
-        if(signupData.email === "") errors.email = true;
-        if(signupData.password === "") errors.password = true;
-        if(signupData.retypepassword === "" || signupData.password !== signupData.retypepassword) errors.retypepassword = true;
+
+        if(signupData.fullname === "")
+            errors.fullname = true;
+
+        if(signupData.phone === "")
+            errors.phone = true;
+
+        if(signupData.email === "")
+            errors.email = true;
+
+        if(signupData.role === "")
+            errors.role = true;
+
+        if(signupData.password === "")
+            errors.password = true;
+
+        if(
+            signupData.retypepassword === "" ||
+            signupData.password !== signupData.retypepassword
+        )
+            errors.retypepassword = true;
+
         setErrorData(errors);
+
         return Object.keys(errors).length > 0;
     }
 
     function validateSignin(){
+
         let errors = {};
-        if(signinData.username === "") errors.username = true;
-        if(signinData.password === "") errors.password = true;
+
+        if(signinData.username === "")
+            errors.username = true;
+
+        if(signinData.password === "")
+            errors.password = true;
+
         setErrorData(errors);
+
         return Object.keys(errors).length > 0;
     }
 
     function signin(){
-        /*Connect backend using callApi() function from lib.js
-        Refer lib.js for callApi() parameters*/
+
         if(validateSignin())
             return;
 
         setIsProgress(true);
-        callApi("POST", apibaseurl + "/authservice/signin", signinData, null, signinResponseHandler);
+
+        callApi(
+            "POST",
+            apibaseurl + "/authservice/signin",
+            signinData,
+            null,
+            signinResponseHandler
+        );
     }
 
     function signup(){
-        /*Connect backend using callApi() function from lib.js
-        Refer lib.js for callApi() parameters*/
+
         if(validateSignup())
             return;
 
         setIsProgress(true);
-        callApi("POST", apibaseurl + "/authservice/signup", signupData, null, signupResponseHandler);
+
+        callApi(
+            "POST",
+            apibaseurl + "/authservice/signup",
+            signupData,
+            null,
+            signupResponseHandler
+        );
     }
 
     function signinResponseHandler(res){
+
         if(res.code != 200)
             alert(res.message);
+
         else{
-            localStorage.setItem("token", res.jwt);     
-            window.location.replace("/home");
-        }  
+
+            localStorage.setItem("token", res.jwt);
+
+            window.location.href = "/home";
+        }
+
         setIsProgress(false);
     }
 
     function signupResponseHandler(res){
+
         alert(res.message);
+
         setIsProgress(false);
+
         setSignupData({
             fullname: "",
             phone: "",
             email: "",
+            role: "",
             password: "",
             retypepassword: ""
         });
+
         finput.current?.focus();
     }
 
     return (
+
         <div className='app'>
+
             <div className='container' key={isSignin ? "signin" : "signup"}>
+
                 <div className='container-header'>
-                    <label>{isSignin ? "Login": "Create Account"}</label>
+
+                    <label>
+                        {isSignin ? "Login": "Create Account"}
+                    </label>
+
                     <img src={imgurl + "logo.png"} alt='' />
+
                 </div>
+
                 <div className='container-content'>
-                    {isSignin? 
+
+                    {
+                        isSignin?
+
                         <>
-                        <label>Username*</label>
-                        <div className='input-group'>
-                            <img src={imgurl + "user.png"} />
-                            <input type='text' ref={finput} className={errorData.username ? 'error' : ''} placeholder='Enter email id' autoComplete='off' name="username" value={signinData.username} onChange={(e)=>handleSigninInput(e)} />
-                        </div>
-                        <label>Password*</label>
-                        <div className='input-group'>
-                            <img src={imgurl + "padlock.png"} />
-                            <input type='password' className={errorData.password ? 'error' : ''} placeholder='Enter password' name='password' value={signinData.password} onChange={(e)=>handleSigninInput(e)} />
-                        </div>
-                        <p>Forgot <span>Password?</span></p>
-                        <button onClick={()=>signin()}>Let's start</button>
-                        <label onClick={()=>switchWindow()}>Don't have an account? <span>Sign up</span></label>
+                            <label>Username*</label>
+
+                            <div className='input-group'>
+
+                                <img src={imgurl + "user.png"} />
+
+                                <input
+                                    type='text'
+                                    ref={finput}
+                                    className={errorData.username ? 'error' : ''}
+                                    placeholder='Enter email id'
+                                    autoComplete='off'
+                                    name="username"
+                                    value={signinData.username}
+                                    onChange={(e)=>handleSigninInput(e)}
+                                />
+
+                            </div>
+
+                            <label>Password*</label>
+
+                            <div className='input-group'>
+
+                                <img src={imgurl + "padlock.png"} />
+
+                                <input
+                                    type='password'
+                                    className={errorData.password ? 'error' : ''}
+                                    placeholder='Enter password'
+                                    name='password'
+                                    value={signinData.password}
+                                    onChange={(e)=>handleSigninInput(e)}
+                                />
+
+                            </div>
+
+                            <p>
+                                Forgot <span>Password?</span>
+                            </p>
+
+                            <button onClick={()=>signin()}>
+                                Let's start
+                            </button>
+
+                            <label onClick={()=>switchWindow()}>
+                                Don't have an account?
+                                <span> Sign up</span>
+                            </label>
+
                         </>
-                    :
+
+                        :
+
                         <>
-                        <label>Full Name*</label>
-                        <div className='input-group'>
-                            <img src={imgurl + "user.png"} />
-                            <input type='text' ref={finput} className={errorData.fullname ? 'error' : ''}  placeholder='Enter full name' autoComplete='off' name='fullname' value={signupData.fullname} onChange={(e)=>handleSignupInput(e)} />
-                        </div>
-                        <label>Mobile Number*</label>
-                        <div className='input-group'>
-                            <img src={imgurl + "phone.png"} />
-                            <input type='text' className={errorData.phone ? 'error' : ''} placeholder='Enter mobile number' autoComplete='off' name='phone' value={signupData.phone} onChange={(e)=>handleSignupInput(e)} />
-                        </div>
-                        <label>Email Address*</label>
-                        <div className='input-group'>
-                            <img src={imgurl + "email.png"} />
-                            <input type='text' className={errorData.email ? 'error' : ''} placeholder='Enter email id' autoComplete='off' name='email' value={signupData.email} onChange={(e)=>handleSignupInput(e)} />
-                        </div>
-                        <label>Password*</label>
-                        <div className='input-group'>
-                            <img src={imgurl + "padlock.png"} />
-                            <input type='password' className={errorData.password ? 'error' : ''} placeholder='Enter password' autoComplete='off' name='password' value={signupData.password} onChange={(e)=>handleSignupInput(e)} />
-                        </div>
-                        <label>Re-type Password*</label>
-                        <div className='input-group'>
-                            <img src={imgurl + "padlock.png"} />
-                            <input type='password' className={errorData.retypepassword ? 'error' : ''} placeholder='Re-type your password' autoComplete='off' name='retypepassword' value={signupData.retypepassword} onChange={(e)=>handleSignupInput(e)} />
-                        </div>
-                        <button onClick={()=>signup()}>Register</button>
-                        <label onClick={()=>switchWindow()}>Already have an account? <span>Sign in</span></label>
+                            <label>Full Name*</label>
+
+                            <div className='input-group'>
+
+                                <img src={imgurl + "user.png"} />
+
+                                <input
+                                    type='text'
+                                    ref={finput}
+                                    className={errorData.fullname ? 'error' : ''}
+                                    placeholder='Enter full name'
+                                    autoComplete='off'
+                                    name='fullname'
+                                    value={signupData.fullname}
+                                    onChange={(e)=>handleSignupInput(e)}
+                                />
+
+                            </div>
+
+                            <label>Mobile Number*</label>
+
+                            <div className='input-group'>
+
+                                <img src={imgurl + "phone.png"} />
+
+                                <input
+                                    type='text'
+                                    className={errorData.phone ? 'error' : ''}
+                                    placeholder='Enter mobile number'
+                                    autoComplete='off'
+                                    name='phone'
+                                    value={signupData.phone}
+                                    onChange={(e)=>handleSignupInput(e)}
+                                />
+
+                            </div>
+
+                            <label>Email Address*</label>
+
+                            <div className='input-group'>
+
+                                <img src={imgurl + "email.png"} />
+
+                                <input
+                                    type='text'
+                                    className={errorData.email ? 'error' : ''}
+                                    placeholder='Enter email id'
+                                    autoComplete='off'
+                                    name='email'
+                                    value={signupData.email}
+                                    onChange={(e)=>handleSignupInput(e)}
+                                />
+
+                            </div>
+
+                            <label>Role*</label>
+
+                            <div className='input-group'>
+
+                                <select
+                                    name='role'
+                                    className={errorData.role ? 'error' : ''}
+                                    value={signupData.role}
+                                    onChange={handleSignupInput}
+                                    style={{
+                                        width:'100%',
+                                        height:'100%',
+                                        border:'none',
+                                        paddingLeft:'10px'
+                                    }}
+                                >
+
+                                    <option value="">
+                                        Select Role
+                                    </option>
+
+                                    <option value="1">
+                                        User
+                                    </option>
+
+                                    <option value="2">
+                                        Manager
+                                    </option>
+
+                                    <option value="3">
+                                        Admin
+                                    </option>
+
+                                    <option value="4">
+                                        Guest
+                                    </option>
+
+                                </select>
+
+                            </div>
+
+                            <label>Password*</label>
+
+                            <div className='input-group'>
+
+                                <img src={imgurl + "padlock.png"} />
+
+                                <input
+                                    type='password'
+                                    className={errorData.password ? 'error' : ''}
+                                    placeholder='Enter password'
+                                    autoComplete='off'
+                                    name='password'
+                                    value={signupData.password}
+                                    onChange={(e)=>handleSignupInput(e)}
+                                />
+
+                            </div>
+
+                            <label>Re-type Password*</label>
+
+                            <div className='input-group'>
+
+                                <img src={imgurl + "padlock.png"} />
+
+                                <input
+                                    type='password'
+                                    className={errorData.retypepassword ? 'error' : ''}
+                                    placeholder='Re-type your password'
+                                    autoComplete='off'
+                                    name='retypepassword'
+                                    value={signupData.retypepassword}
+                                    onChange={(e)=>handleSignupInput(e)}
+                                />
+
+                            </div>
+
+                            <button onClick={()=>signup()}>
+                                Register
+                            </button>
+
+                            <label onClick={()=>switchWindow()}>
+                                Already have an account?
+                                <span> Sign in</span>
+                            </label>
+
                         </>
                     }
+
                 </div>
-                <div className='container-footer'>Copyright - 30334 @ 2026. All rights reserved.</div>
+
+                <div className='container-footer'>
+                    Copyright-30334@ 2026. All rights reserved.
+                </div>
+
             </div>
 
             <ProgressBar isProgress={isProgress}/>
+
         </div>
     );
 }
--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,62 +1,110 @@
-import React, { useEffect, useState } from 'react';
-import './Home.css';
-import { apibaseurl, callApi, imgurl } from '../lib';
-import ProgressBar from './ProgressBar';
+import "./Home.css";
 
 const Home = () => {
-    const [fullname, setFullname] = useState("");
-    const [isProgress, setIsProgress] = useState("");
-    const [token, setToken] = useState("");
-    const [menuList, setMenuList] = useState([]);
-
-    useEffect(()=>{
-        const token = localStorage.getItem("token");
-        if(!token)
-            logout();
-        else{
-            setToken(token);
-            setIsProgress(true);
-            callApi("GET", apibaseurl + "/authservice/uinfo", null, null, loadUinfo, token);
-        }
-    }, []);
-
-    function loadUinfo(res){
-        setIsProgress(false);
-        if(res.code != 200)
-            return;
-        setFullname(res.fullname);
-        setMenuList(res.menulist);
-    }
-
-    function logout(){
-        localStorage.clear();
-        window.location.replace("/");
-    }
 
     return (
-        <div className='home'>
-            <div className='home-header'>
-                <img src="/logo.png" alt='' />
-                <div className='info'>
-                    {fullname}
-                    <img src="/shutdown.png" alt='' onClick={()=>logout()} />
-                </div>
-            </div>
-            <div className='home-workspace'>
-                <div className='home-menus'>
-                    <ul>
-                        {menuList.map((m)=>(
-                            <li><img src={imgurl + m.icon} alt='' />{m.menu}</li>
-                        ))}
-                    </ul>
-                </div>
-                <div className='home-content'>Content</div>
-            </div>
-            <div className='home-footer'>Copyright @ 2026. All rights reserved.</div>
+        <div className="home-container">
 
-            <ProgressBar isProgress={isProgress}/>
+            {/* HEADER */}
+            <div className="topbar">
+                <div className="logo-section">
+                    <img
+                        src="http://localhost:5173/logo.png"
+                        alt="logo"
+                    />
+                 
+                </div>
+
+                <div className="user-section">
+                    <span>Roshini</span>
+                    <button className="logout-btn">⏻</button>
+                </div>
+            </div>
+
+            {/* BODY */}
+            <div className="main-layout">
+
+                {/* SIDEBAR */}
+                <div className="sidebar">
+
+                    <ul>
+                        <li className="active">📊 Dashboard</li>
+                        <li>📝 My Task</li>
+                        <li>⚙️ Task Manager</li>
+                        <li>👤 User Manager</li>
+                        <li>🙍 My Profile</li>
+                        <li className="role-text">Roles</li>
+                    </ul>
+
+                </div>
+
+                {/* CONTENT */}
+                <div className="content-area">
+
+                    {/* ROLES */}
+                    <div className="card">
+
+                        <h3>Roles</h3>
+
+                        <div className="input-row">
+                            <input type="text" placeholder="Enter role" />
+                            <button>Add Role</button>
+                        </div>
+
+                    </div>
+
+                    {/* MENU */}
+                    <div className="card">
+
+                        <h3>Menu</h3>
+
+                        <div className="input-row">
+                            <input type="text" placeholder="Enter menu" />
+                            <button>Add</button>
+                        </div>
+
+                    </div>
+
+                    {/* MAP MENU */}
+                    <div className="card">
+
+                        <h3>Map Menu with Roles</h3>
+
+                        <div className="map-container">
+
+                            <div className="left-side">
+
+                                <select>
+                                    <option>Select Role</option>
+                                    <option>User</option>
+                                    <option>Manager</option>
+                                    <option>Admin</option>
+                                </select>
+
+                            </div>
+
+                            <div className="right-side">
+
+                                <label><input type="checkbox" /> MyTask</label>
+                                <label><input type="checkbox" /> Task Manager</label>
+                                <label><input type="checkbox" /> User Manager</label>
+                                <label><input type="checkbox" /> Role Manager</label>
+                                <label><input type="checkbox" /> My Profile</label>
+
+                                <button>Add</button>
+
+                            </div>
+
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+
         </div>
     );
-}
+};
 
 export default Home;
